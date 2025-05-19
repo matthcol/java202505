670f59f527df9b584da17b037532336b24248627
matthcol <matthias.colin@gmail.com>
2025-05-19 11:00:19 +0200

    first commit

---
+++ basics/src/main/java/basics/Application.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Application {
+    public static void main(String[] args) {
+        // Sortie standard
+        System.out.println("Coucou c'est lundi !");
+        System.out.println("Apprenons Java ensemble");
+
+        // Sortie d'erreur
+        System.err.println("Ceci est une erreur !");
+    }
+}
