aafa038783e405bac919e516caab57210c474ae4
matthcol <matthias.colin@gmail.com>
2025-05-23 12:07:39 +0200

    JPA settings

---
+++ cityspringapp/src/main/java/org/example/city/repository/CityRepository.java
@@ -0,0 +1,9 @@
+package org.example.city.repository;
+
+import org.example.city.entity.CityFr;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<CityFr, Integer> {
+
+    // Custom Queries
+}
